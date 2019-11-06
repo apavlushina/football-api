5e97eddc2dd019895351d21a645a549977ef4440
apavlushina <ssemina7@gmail.com>
2019-11-06 14:52:10 +0100

    added post method

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log("listening on port " + port));
 
-const db = require("./db");
+const teamRouter = require("./team/router");
+
+app.use(teamRouter);
--- team/router.js
+++ team/router.js
@@ -9,4 +9,10 @@ router.get("/team", (request, response, next) => {
     .catch(err => next(err));
 });
 
+router.post("/team", (req, res, next) => {
+  Team.create(req.body)
+    .then(name => res.json(name))
+    .catch(err => next(err));
+});
+
 module.exports = router;
